3b50643bfa6897564b43b0d3878d58615d4bc8bf
wilfriedmpondo16-max <wilfriedmpondo16@gmail.com>
2025-11-04 17:27:56 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "blinder~cMQ2iJAS#gU9FuF6MeD2WXpwJF5gon4ODhjC0O5OI4b9fDnx_yFo",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~YF40ECgT#eyj6uLBcglImGAHvdatt3Z9vcUmKyG-O9TgYRFBBSOc",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
